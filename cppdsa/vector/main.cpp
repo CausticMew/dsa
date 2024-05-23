496db175300144e542247a7e1e004eb3537ec4ee
Hooly <hooly@julios-air.mynetworksettings.com>
2024-05-23 15:39:06 -0400

    Tested add and print functions

--- cppdsa/vector/main.cpp
+++ cppdsa/vector/main.cpp
@@ -6,5 +6,18 @@ int main() {
 
     List<std::string> vect;
 
-    vect.push("word", 0);
+//add test
+    vect.push("I", 0);
+    vect.push("am", 1);
+    vect.push("a", 2);
+    vect.push("dormant", 3);
+    vect.push("volcano", 4);
+    vect.push("and", 5);
+    vect.push("I", 6);
+    vect.push("love", 7);
+    vect.push("lava", 8);
+    vect.push("cakes", 9);
+    vect.print();
+    vect.push("Hello,", 0);
+    vect.print();
 }
