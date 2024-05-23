2900ff603a73bbe4ac2ec431a6780e2685ab6133
Hooly <hooly@julios-air.mynetworksettings.com>
2024-05-23 14:40:05 -0400

    Implemented add method untested

--- cppdsa/vector/list.hpp
+++ cppdsa/vector/list.hpp
@@ -8,17 +8,27 @@ class List{
     private:
         T arr[];
         int size;
-        
+        int MAX;
+
     public:
         //constructor
         List() {
+            this->MAX = 10;
+            this->size = 0;
+            this->arr = new T[MAX];
+        }
 
+        //custom constructor
+        List(int CAP) {
+            this->MAX = CAP;
+            this->size = 0;
+            this->arr = new T[CAP];
         }
 
         //copy constructor
-        List(const T &other[]) {
-
-        }
+        // List(const T &other[]) {
+            
+        // }
 
         //copy constructor mimic
         void copy(const T &other[]) {
@@ -26,9 +36,12 @@ class List{
         }
 
         //operator = 
-        const operator= (const T &other) {
-
-        }
+        // const operator= (const T &other) {
+        //     if (this != &other) {
+        //         destructor();
+        //         copy(other);
+        //     }
+        // }
 
         //destructor
         ~List() {
@@ -42,17 +55,43 @@ class List{
 
         //isEmpty
         bool isEmpty() {
-
+            // if (this->size == 0) {
+            //     return true;
+            // }
+            // return false;
+            return size == 0;
         }
 
         //size/length
         int size() {
-
+            return size;
         }
 
         //push
         void push(T element, int index) {
+            if (index < 0 || index >= size) {
+                throw std::out_of_range("Index " + std::to_string(index) + " is out of bounds.");
+            }
 
+            if (isEmpty() && index != 0) {
+                std::cout << "Array is empty, index: " << index << " is greater than 0.\n";
+                return;
+            }
+
+            if (MAX <= size) {
+                T temp = new T[MAX *= 2];
+                for (int i = 0; i < size; i++) {
+                    temp[i] = this->arr[i];
+                }
+                this->arr = temp;
+            }
+
+            for (int i = size; i > index; i--) {
+                    arr[i] == arr[i - 1];
+            }
+            this->arr[index] = element;
+
+            size++;
         }
 
         //remove
@@ -83,6 +122,3 @@ class List{
 
 
 #endif
-
-
-
