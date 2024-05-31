d7857a0f6b40ec3b0e5bc2917de23c3a956d92d6
CausticMew <julio174@gmail.com>
2024-05-30 20:50:30 -0400

    Implemented and tested push and print functions

--- cppdsa/linkedList/list.hpp
+++ cppdsa/linkedList/list.hpp
@@ -1,29 +1,74 @@
 #ifndef LIST_HPP
 #define LIST_HPP
+#include "node.hpp"
 
 template<typename T>
 class List {
     private:
-        node<T>* start;
+        node<T>* head;
         int size;
 
     public:
     //default constructor
+    List() {
+        this->head = nullptr;
+        this->size = 0;
+    }
+
     //copy constructor
+
     //copy mimic
+
     //destructor
+
     //destructor mimic
+
     //operator overload = 
+
     //get
+
     //set
+
     //isEmpty
+    bool isEmpty() {
+        return this->head == nullptr;
+    }
+
     //push
+    void push(T element) {
+        node<T>* pushed = new node<T>(element);
+        if (this->head == nullptr) {
+            this->head = pushed;
+        } else {
+            node<T>* walker = head;
+            while(walker->next != nullptr) {
+                walker = walker->next;
+            }
+            walker->next = pushed;
+        }
+    }
+
     //addAt
+
+
     //remove
+
     //clear
+
     //indexOf
+
     //indexesOf
+
     //reverse
+
+    //print
+    void print() {
+        node <T>* walker = head;
+        while(walker != nullptr) {
+            std::cout << walker->value << ' ';
+            walker = walker->next;
+        }
+    }
 };
 
 #endif
--- cppdsa/linkedList/main.cpp
+++ cppdsa/linkedList/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-#include "List.hpp"
+#include "list.hpp"
 
 int main() {
-    
+    List <int> l;
+
+    //push test
+    l.push(7);
+    l.push(11);
+
+    //print test
+    l.print();
 }
